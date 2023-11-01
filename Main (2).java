33931997bed7e368dcb7c5b39f6677e99ce98720
chandansikarwar <144825080+chandansikarwar@users.noreply.github.com>
2023-11-01 12:41:14 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,15 @@
+public class Main extends Thread
+{
+	public static void main(String[] args) {
+		Main t1 = new Main();
+		t1.start();
+		Main t2 = new Main();
+		t2.start();
+		Main t3 = new Main();
+		t3.start();
+		System.out.println("this code is outside of the thread:");
+	}
+	public void run(){
+	    System.out.println("this code is running in a thread :");
+	}
+}
